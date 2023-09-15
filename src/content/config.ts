0388feae1b91ac2b84af9bee05ba68237b6b0c7c
OwlthTech <owlthtech@gmail.com>
2023-09-15 23:04:15 +0530

    new file:   src/components/shop/Grid.astro 	new file:   src/components/shop/GridItem.astro 	new file:   src/components/shop/Headline.astro 	new file:   src/components/shop/List.astro 	new file:   src/components/shop/ListItem.astro 	new file:   src/components/shop/Pagination.astro 	new file:   src/components/shop/SinglePost.astro 	new file:   src/components/shop/Tags.astro 	new file:   src/components/shop/ToBlogLink.astro 	modified:   src/components/widgets/Hero.astro 	modified:   src/config.yaml 	modified:   src/content/config.ts 	new file:   src/content/product/product-1.mdx 	new file:   src/content/product/product-2.md 	new file:   src/content/product/product-3.md 	new file:   src/content/product/product-4.mdx 	new file:   src/content/product/product-5.md 	new file:   src/content/product/product-landing.md 	modified:   src/navigation.js 	modified:   src/pages/[...blog]/[...page].astro 	new file:   src/pages/[...shop].zip 	new file:   src/pages/[...shop]/[...page].astro 	new file:   src/pages/[...shop]/[category]/[...page].astro 	new file:   src/pages/[...shop]/[tag]/[...page].astro 	new file:   src/pages/[...shop]/index.astro 	modified:   src/types.d.ts 	modified:   src/utils/config.ts 	modified:   src/utils/permalinks.ts 	new file:   src/utils/product.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -63,6 +63,24 @@ const postCollection = defineCollection({
   }),
 });
 
+const productCollection = defineCollection({
+  schema: z.object({
+    publishDate: z.date().optional(),
+    updateDate: z.date().optional(),
+    draft: z.boolean().optional(),
+
+    title: z.string(),
+    excerpt: z.string().optional(),
+    image: z.string().optional(),
+
+    category: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+
+    metadata: metadataDefinition(),
+  }),
+});
+
 export const collections = {
   post: postCollection,
+  product: productCollection,
 };
